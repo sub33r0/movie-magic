a87b912bde29981e0516e435a32ae43d1ca16e31
sub33r0 <z3rowords@gmail.com>
2024-01-19 23:23:05 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,9 @@ const movieController = require('./controllers/movieController');
 router.use(homeController);
 router.use(movieController);
 
+
+router.get('*', (req, res) => {
+    res.redirect('/404')
+})
+
 module.exports = router;
