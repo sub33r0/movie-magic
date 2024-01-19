cd2f3abcd71f55fb631679ee9ebf2c924ff2cfc5
sub33r0 <z3rowords@gmail.com>
2024-01-19 13:13:21 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}...`)
+})
