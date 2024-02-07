2c1379b5df88cd362690dba41cea5d01e9168cb7
sub33r0 <z3rowords@gmail.com>
2024-02-07 17:34:11 +0200

    Add edit movie page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -47,7 +47,8 @@ router.post('/movies/:movieId/attach', async (req, res) => {
 });
 
 router.get('/movies/:movieId/edit', async (req, res) => {
-    res.render('movie/edit')
+    const movie = await movieService.getOne(req.params.movieId).lean();
+    res.render('movie/edit', { movie })
 });
 
 module.exports = router;
